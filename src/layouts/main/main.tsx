054e07ddb02fd86b3306eb15cef7dbaad4c1052a
GlebChiz <gchyz@jsdaddy.io>
2022-06-11 11:14:12 +0300

    feature: added globaks.d.ts and button component

--- src/layouts/main/main.tsx
+++ src/layouts/main/main.tsx
@@ -1,5 +1,12 @@
+import Button from "../../components/button/button";
+
 function Main() {
-  return <h1>Main</h1>;
+  return (
+    <>
+      <h1>Main</h1>
+      <Button text="text" />
+    </>
+  );
 }
 
 export default Main;
