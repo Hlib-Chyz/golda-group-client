b790895a360bbf5d272c1f671f5a821f306135ce
GlebChiz <gchyz@jsdaddy.io>
2022-07-30 10:41:44 +0300

    feature: remove old price

--- src/layouts/main/components/tariffs/tariffs.tsx
+++ src/layouts/main/components/tariffs/tariffs.tsx
@@ -14,7 +14,7 @@ function Tariffs() {
       <div className={styles.tariffs}>
         <Tariff
           oldPrice="690"
-          newPrice="590"
+          newPrice="690"
           img={homeEducation}
           backgroundColor="#EBF8FF"
           whatHaveThisTariff={[
@@ -40,7 +40,7 @@ function Tariffs() {
         />
         <Tariff
           oldPrice="1490"
-          newPrice="1290"
+          newPrice="1490"
           img={freelancer}
           backgroundColor="rgba(255, 221, 189, 0.9)"
           whatHaveThisTariff={[
@@ -73,7 +73,7 @@ function Tariffs() {
         />
         <Tariff
           oldPrice="2990"
-          newPrice="2790"
+          newPrice="2990"
           img={datingChat}
           backgroundColor="#FFA857"
           whatHaveThisTariff={[
